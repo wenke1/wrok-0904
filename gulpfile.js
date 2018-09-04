df93ae91de01c65e2eac5ea69569ae3b0bc91686
lxy <3024772560@qq.com>
2018-09-04 20:32:47 +0800

    better完成-lxy

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,14 @@
 var gulp = require("gulp");
 var sass = require("gulp-sass");
 var minCss = require("gulp-clean-css");
+var minJS = require("gulp-uglify");
+var server = require("gulp-webserver");
+
+var fs = require("fs");
+var path = require("path");
+var url = require("url");
+
+var data = require("./src/json/data");
 
 gulp.task("sass", function() {
     return gulp.src("./src/scss/*.scss")
@@ -8,3 +16,30 @@ gulp.task("sass", function() {
         .pipe(minCss())
         .pipe(gulp.dest("./src/css"));
 });
+
+gulp.task("watch", function() {
+    return gulp.watch("./src/scss/*.scss", gulp.series("sass"));
+})
+
+
+gulp.task("devServer", function() {
+    return gulp.src("src")
+        .pipe(server({
+            port: 8989,
+            middleware: function(req, res) {
+                if (req.url == "/favicon.ico") {
+                    return res.end();
+                }
+                var pathname = url.parse(req.url).pathname;
+
+                if (pathname == "/api/data") {
+                    res.end(JSON.stringify({ code: 1, mes: data }));
+                } else {
+                    pathname = pathname === "/" ? "index.html" : pathname;
+                    res.end(fs.readFileSync(path.join(__dirname, "src", pathname)));
+                }
+            }
+        }))
+})
+
+gulp.task("dev", gulp.series("sass", "devServer", "watch"));
